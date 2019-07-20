9c74e03826f4bbc05f4ab0bffb19891f30c5e601
KenNguch <kennguch3@gmail.com>
2019-07-20 19:59:14 +0300

    Added Login with Firebase

--- app/src/main/java/kennguch/github/instagram/LoginActivity.java
+++ app/src/main/java/kennguch/github/instagram/LoginActivity.java
@@ -1,17 +1,29 @@
 package kennguch.github.instagram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-    private TextView mNotAMember;;
+    private EditText mPassword, mEmail;
+    private TextView mNotAMember;
+    private FirebaseAuth mAuth;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +36,60 @@ public class LoginActivity extends AppCompatActivity {
         mNotAMember.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doLogin();
+            }
+        });
+
+
 
 
     }
+
+    private void doLogin() {
+
+        String email = mEmail.getText().toString().trim();
+
+        String password = mPassword.getText().toString().trim();
+
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Succefully Logged In", task.getException().toString());
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d("signInWithEmail:failure", task.getException().toString());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 
     private void initializer() {
-
-
+        mPassword = findViewById(R.id.login_passWord);
+        mEmail = findViewById(R.id.login_email);
         mNotAMember = findViewById(R.id.not_a_member_yet);
+        mAuth = FirebaseAuth.getInstance();
+        mButton = findViewById(R.id.btn_login);
+
     }
+
 
 }
